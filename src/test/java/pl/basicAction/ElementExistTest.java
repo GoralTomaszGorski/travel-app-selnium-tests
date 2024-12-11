641ddaa12677f9fee25e3b8ca07c1254cf4c11e0
tomaszgorski <tomasz.gorski88@gmail.com>
2024-12-11 22:47:55 +0100

    element: isDisplayed, isSelected, getAttribute img

--- src/test/java/pl/basicAction/ElementExistTest.java
+++ src/test/java/pl/basicAction/ElementExistTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -18,11 +19,29 @@ public class ElementExistTest {
         driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
 
+        System.out.println("we manually check that element exist by try catch");
         System.out.println(elementExist(By.tagName("p")));
         System.out.println(elementExist(By.id("topSecret")));
 
+        System.out.println("we manually check that element exist by list size");
         System.out.println(elementExistSecond(By.tagName("p")));
         System.out.println(elementExistSecond(By.id("topSecret")));
+
+        System.out.println("we check that element exist by isDisplayed method");
+        System.out.println(driver.findElement(By.tagName("p")).isDisplayed());
+        System.out.println(driver.findElement(By.tagName("button")).isDisplayed());
+
+        System.out.println("we check that element is enabled by isEnabled method");
+        System.out.println(driver.findElement(By.tagName("p")).isEnabled());
+        System.out.println(driver.findElement(By.tagName("button")).isEnabled());
+
+        System.out.println("we check that element is isSelected example checkbox");
+        WebElement checkbox = driver.findElement(By.cssSelector("[type='checkbox']"));
+        System.out.println(checkbox.isSelected());
+
+        //sprwdzenie rzomiaru zdjecia mozna inne atrubuty sprawdzic
+        WebElement image = driver.findElement(By.tagName("img"));
+        System.out.println(image.getAttribute("naturalHeight"));
     }
 
     public boolean elementExist(By locator){
@@ -38,7 +57,4 @@ public class ElementExistTest {
     public boolean elementExistSecond(By locator){
         return driver.findElements(locator).size() > 0;
     }
-
-
-
 }
