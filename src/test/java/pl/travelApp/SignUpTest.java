a89c4d0ab8f6b1c7c24f206460a014b153927577
tomaszgorski <tomasz.gorski88@gmail.com>
2025-01-06 20:36:56 +0100

    sign up test

---
+++ src/test/java/pl/travelApp/SignUpTest.java
@@ -0,0 +1,32 @@
+package pl.travelApp;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static org.testng.Assert.assertEquals;
+
+public class SignUpTest {
+
+    @Test
+    public void signUp() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+
+        Thread.sleep(5000);
+        driver.quit();
+
+    }
+}
