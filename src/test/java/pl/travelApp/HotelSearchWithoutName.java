93f26e9c6930ed93bce37192ce99108705c116d7
tomaszgorski <tomasz.gorski88@gmail.com>
2025-01-11 22:17:16 +0100

    add Hotel Search Without Name

---
+++ src/test/java/pl/travelApp/HotelSearchWithoutName.java
@@ -0,0 +1,63 @@
+package pl.travelApp;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static org.testng.Assert.assertEquals;
+
+public class HotelSearchWithoutName {
+
+    @Test
+    public void hotelSearch() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        WebElement checkin = driver.findElement(By.name("checkin"));
+        checkin.sendKeys("17/01/2025");
+        checkin.click();
+        WebElement checkout = driver.findElement(By.name("checkout"));
+        checkout.sendKeys("25/01/2025");
+
+        driver.findElements(By.xpath(
+                        "//td[@class='day ' and text()='26']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst().ifPresent(WebElement::click);
+
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+        List<String> hotelsName = driver.findElements(
+                        By.xpath("//h4[contains(@class, 'list_title')]//b"))
+                .stream()
+                .map(element ->
+                        element.getDomProperty("textContent"))
+                .collect(Collectors.toList());
+        System.out.println(hotelsName.size());
+        hotelsName.forEach(el -> System.out.println(el));
+        WebElement noResultHeading = driver.findElement(By.xpath("//div[@class='itemscontainer']//h2"));
+        System.out.println(noResultHeading.getText());
+        Assert.assertTrue(noResultHeading.isDisplayed());
+        Assert.assertEquals("No Results Found", noResultHeading.getText());
+
+
+        Thread.sleep(1000);
+        driver.quit();
+
+    }
+}
