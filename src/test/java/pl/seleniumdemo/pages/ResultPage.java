0e9fec97ece9c5b988b72a9d45fcbaf76384fead
tomaszgorski <tomasz.gorski88@gmail.com>
2025-01-23 22:22:27 +0100

    getHotelsName method

--- src/test/java/pl/seleniumdemo/pages/ResultPage.java
+++ src/test/java/pl/seleniumdemo/pages/ResultPage.java
@@ -1,6 +1,24 @@
 package pl.seleniumdemo.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pl.seleniumdemo.tests.BaseTest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ResultPage extends BaseTest {
+
+    @FindBy(xpath="//h4[contains(@class, 'list_title')]//b")
+    private List<WebElement> hotelList;
+
+    public List<String> getHotelsName(){
+        return hotelList.stream()
+                .map(element -> element.getDomAttribute("textContent"))
+                .collect(Collectors.toList());
+    }
+
+
+
 }
