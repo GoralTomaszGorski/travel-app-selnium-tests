6933590bb9f8c696f41d52807df5bd1e1c507426
tomaszgorski <tomasz.gorski88@gmail.com>
2025-01-27 22:53:16 +0100

    add new selector parameters

--- src/test/java/pl/seleniumdemo/pages/HotelSearchPage.java
+++ src/test/java/pl/seleniumdemo/pages/HotelSearchPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class HotelSearchPage {
 
     @FindBy(xpath = "//span[text()='Search by Hotel or City Name']")
@@ -38,11 +40,11 @@ public class HotelSearchPage {
     @FindBy(xpath = "//button[text()=' Search']")
     private WebElement searchButton;
 
-    @FindBy(xpath = "//li[id=' li_myaccount']")
-    private WebElement myAccount;
+    @FindBy(xpath = "//*[@id='li_myaccount']")
+    private List<WebElement> myAccountLink;
 
-    @FindBy(xpath = "//a[text()=' Sign Up']")
-    private WebElement signUpLink;
+    @FindBy(linkText = "Sign Up")
+    private List<WebElement> signUpLink;
 
     private WebDriver driver;
 
