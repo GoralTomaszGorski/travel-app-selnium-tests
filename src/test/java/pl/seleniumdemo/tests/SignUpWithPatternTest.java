b0b1ce53f8dc07aa460d742684b99879f471b73c
tomaszgorski <tomasz.gorski88@gmail.com>
2025-01-27 22:55:42 +0100

    SignUpWithPatternTest class

---
+++ src/test/java/pl/seleniumdemo/tests/SignUpWithPatternTest.java
@@ -0,0 +1,13 @@
+package pl.seleniumdemo.tests;
+
+
+import org.testng.annotations.Test;
+
+public class SignUpWithPatternTest extends BaseTest{
+
+    @Test
+    public void signUp() throws InterruptedException {
+
+
+    }
+}
