83703081e4f6aeef9bffec6e030de1518d23a3cd
Tomasz Gorski <t.gorski@posnet.com>
2025-01-30 07:45:00 +0100

    selectors

--- src/test/java/pl/seleniumdemo/pages/SignUpPage.java
+++ src/test/java/pl/seleniumdemo/pages/SignUpPage.java
@@ -7,13 +7,36 @@ import org.openqa.selenium.support.PageFactory;
 
 public class SignUpPage {
 
-
+    @FindBy (name = "firstname")
+    private WebElement firstnameInput;
+    
+    @FindBy (name = "lastname")
+    private WebElement lastnameInput;
+    
+    @FindBy (name = "phone")
+    private WebElement phoneInput;
+    
+    @FindBy (name = "email")
+    private WebElement emailInput;
+    
+    @FindBy (name = "password")
+    private WebElement passwordInput;
+    
+    @FindBy (name = "confirmpassword")
+    private WebElement confirmpasswordInput;
+    
+    @FindBy (name = "signupbtn")
+    private WebElement signupbtnBtn;
 
     public SignUpPage(WebDriver driver){
         PageFactory.initElements(driver, this);
         
     }
 
+    public void setFirstname(String firstname){
+        firstnameInput.sendKeys();
+
+    }
 
 
 }
