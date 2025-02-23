edc726083edb477f7e51942cbcb071317c9535ed
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-02-23 16:47:34 +0100

    screenshot method add

--- src/test/java/pl/utils/SeleniumHelper.java
+++ src/test/java/pl/utils/SeleniumHelper.java
@@ -1,12 +1,17 @@
 package pl.utils;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.model.Media;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SeleniumHelper {
 
@@ -23,5 +28,24 @@ public class SeleniumHelper {
     public static void waitForNotEmptyList(WebDriver driver, By locator) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1));
         wait.until(browser -> browser.findElements(locator).size() > 0);
+    }
+
+    private static String takeScreenshot(WebDriver driver) throws IOException {
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File file = screenshot.getScreenshotAs(OutputType.FILE);
+        String pathScreen = "src/test/resources/screenshoots/screen" + getCurrentTimestamp() + ".jpg";
+        FileUtils.copyFile(file, new File(pathScreen));
+        return pathScreen;
+    }
+
+    public static String getCurrentTimestamp() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss");
+        return LocalDateTime.now().format(formatter);
+    }
+
+
+    public static Media getScreenshot(WebDriver driver) throws IOException {
+        String path = takeScreenshot(driver);
+        return MediaEntityBuilder.createScreenCaptureFromPath(path).build();
     }
 }
